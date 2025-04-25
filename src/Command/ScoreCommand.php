98546673bd93a4e52b92fbf6b78db64f85cd066e
Alexandr Shmidt <alexandr_shmidt98@vk.com>
2025-04-25 18:28:26 +0700

    Обновил консольную команду. Переложил логику в отдельный класс

--- src/Command/ScoreCommand.php
+++ src/Command/ScoreCommand.php
@@ -4,16 +4,13 @@ namespace App\Command;
 
 
 use App\Repository\UserRepository;
-use App\Service\ScoreService;
 use App\Service\TotalScore;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Doctrine\ORM\EntityManagerInterface;
 
 #[AsCommand(
     name: 'app:score',
@@ -22,7 +19,7 @@ use Doctrine\ORM\EntityManagerInterface;
 class ScoreCommand extends Command
 {
     public function __construct(private UserRepository $userRepository,
-    private EntityManagerInterface $em)
+    private TotalScore $totalScore)
     {
 
         parent::__construct();
@@ -50,13 +47,8 @@ class ScoreCommand extends Command
 
             $io->note(sprintf("Score to user witch id $userId: %s", $score));
         } else {
-            $users = $this->userRepository->findAll();
-            $score = 0;
-            foreach ($users as $user) {
-                $score += $user->getUserScore()->getScore();
-            }
 
-            $io->note(sprintf("Sum: %s", $score));
+            $io->note(sprintf("Sum: %s", $this->totalScore->getTotalScore()));
         }
 
         return Command::SUCCESS;
--- src/Service/TotalScore.php
+++ src/Service/TotalScore.php
@@ -2,20 +2,19 @@
 
 namespace App\Service;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 
 class TotalScore {
     private $score;
     private $users;
 
-    public function __construct(private UserRepository $userRepository, private EntityManagerInterface $em) {
+    public function __construct(private UserRepository $userRepository) {
         $this->score = 0;
         $this->users = $this->userRepository->findAll();
     }
 
     public function getTotalScore(): int {
         foreach ($this->users as $user) {
-            $this->score = $user->getUserScore();
+            $this->score += $user->getUserScore()->getScore();
         }
         return $this->score;
     }
