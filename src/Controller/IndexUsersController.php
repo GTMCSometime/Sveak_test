1687dcd92d0699d681168af306b24a1a21493d65
Alexandr Shmidt <alexandr_shmidt98@vk.com>
2025-04-26 16:41:14 +0700

    установил фикстуры

--- src/Controller/IndexUsersController.php
+++ src/Controller/IndexUsersController.php
@@ -3,20 +3,29 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class IndexUsersController extends AbstractController
 {
     #[Route('/users', name: 'index_users')]
-    public function show(UserRepository $userRepository): Response
+    public function show(Request $request, UserRepository $userRepository, PaginatorInterface $paginator): Response
     {
-        $users = $userRepository->findAll();
+        $query = $userRepository->createQueryBuilder('u')
+        ->orderBy('u.id', 'ASC');
 
+        $pagination = $paginator->paginate(
+            $query, 
+            $request->query->getInt('page', 1), 
+            10 
+        );
+    
         return $this->render('users/index.html.twig', [
-            'users' => $users,
+            'pagination' => $pagination,
         ]);
     }
 }
